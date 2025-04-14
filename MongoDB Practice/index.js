fecbb0cc4fbb30ad1f702d159fa0f652295c4da7
varun-2024 <v.talwar2024@gmail.com>
2025-04-14 17:15:18 +0100

    Insert, Find, Update, Delete Users in MongoDB using Mongoose

--- MongoDB Practice/index.js
+++ MongoDB Practice/index.js
@@ -25,7 +25,8 @@ User1 = new User({
   email: "john.doe@example.com",
   age: 30,
 });
-// Saving User1 to the database
+
+/* // Saving User1 to the database
 User1.save()
   .then((res) => console.log("User saved to database\n", res))
   .catch((err) => console.log(err));
@@ -36,7 +37,45 @@ User.insertMany([
   { name: "Bob Brown", email: "bob.brown@example.com", age: 35 },
 ])
   .then((res) => console.log("Users saved to database\n", res))
+  .catch((err) => console.log(err)); */
+
+//Finding a user in the database
+/* User.find({ age: { $gt: 31 } })
+  .then((res) => {
+    console.log("Users found in database\n", res[0].name);
+  })
   .catch((err) => console.log(err));
+ */
+
+//Fine one and update a user in the database
+/* User.findOneAndUpdate({ name: "John Doe" }, { age: 35 }, { new: true })
+  .then((res) => console.log("User updated in database\n", res))
+  .catch((err) => console.log(err)); */
+
+//Update One User in the database
+/* User.updateOne({ name: "Bob Brown" }, { age: 40 })
+  .then((res) => console.log("User updated in database\n", res))
+  .catch((err) => console.log(err)); */
+
+// Updating multiple users in the database
+/* User.updateMany({ age: { $lte: 30 } }, { $set: { age: 40 } })
+  .then((res) => console.log("User updated in database\n", res))
+  .catch((err) => console.log(err)); */
+
+//Delete a user in the database
+/* User.deleteOne({ name: "John Doe" })
+  .then((res) => console.log("User deleted from database\n", res))
+  .catch((err) => console.log(err)); */
+
+//Deleting multiple users in the database
+/* User.deleteMany({ name: "John Doe" })
+  .then((res) => console.log("User deleted from database\n", res))
+  .catch((err) => console.log(err)); */
+
+//Deleting a user by ID in the database
+/* User.findByIdAndDelete("67fd1cd7ecd5b720afc37397")
+  .then((res) => console.log("User deleted from database\n", res))
+  .catch((err) => console.log(err)); */
 
 /* const db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
